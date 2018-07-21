6cd6f597b5e83804f744e41b887762b5a63a6022
julianknodt <julianknodt@gmail.com>
2018-07-21 12:03:12 -0700

    Cleaned up paths and also added better messages

--- main.go
+++ main.go
@@ -1,57 +1,65 @@
 package main
 
 import (
-  "os"
-  "flag"
-  "fmt"
-  "bufio"
-  "io/ioutil"
-  "net/url"
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+  "os/user"
+  "path/filepath"
 )
 
 func isURL(s string) bool {
-  if _, err := url.ParseRequestURI(s); err != nil {
-    return false
-  }
-  return true
+	if _, err := url.ParseRequestURI(s); err != nil {
+		return false
+	}
+	return true
 }
 
 func main() {
-  savePath := flag.String("save", "../../saved_music.txt", "Location of place to save music")
-  output := flag.Bool("o", false, "Output list instead of writing to it")
-  flag.Parse()
-
-  if *output {
-    data, err := ioutil.ReadFile(*savePath)
-    if err != nil {
-      panic(err)
-    }
-    fmt.Println(string(data))
-  } else {
-    var file *os.File
-    if _, err := os.Stat(*savePath); os.IsNotExist(err) {
-      file, err = os.Create(*savePath)
-      if err != nil {
-        panic(err)
-      }
-    } else {
-      file, err = os.OpenFile(*savePath, os.O_APPEND | os.O_WRONLY, 0666)
-      if err != nil {
-        panic(err)
-      }
-    }
-
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    in := scanner.Bytes()
-    if !isURL(string(in)) {
-      fmt.Println("Not a URL, exitting...")
-      return
-    }
-    _, err := file.WriteString(string(in) + "\n")
-    if err != nil {
-      panic(err)
-    }
-    fmt.Printf("Wrote: %s \n", string(in))
+  usr, err := user.Current()
+  if err != nil {
+    panic(err)
   }
+	savePath := flag.String("save", filepath.Join(usr.HomeDir, "saved_music.txt"), "Location of place to save music")
+	output := flag.Bool("o", false, "Output list instead of writing to it")
+	flag.Parse()
+
+	if *output {
+		data, err := ioutil.ReadFile(*savePath)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(data))
+	} else {
+		var file *os.File
+		if _, err := os.Stat(*savePath); os.IsNotExist(err) {
+      fmt.Printf("Created saved_music.txt in %s \n", *savePath)
+			file, err = os.Create(*savePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			file, err = os.OpenFile(*savePath, os.O_APPEND|os.O_WRONLY, 0666)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			in := scanner.Text()
+			if !isURL(in) {
+				fmt.Println("Not a URL, skipping...")
+				continue
+			}
+			_, err := file.WriteString(in + "\n")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Wrote: %s \n", in)
+		}
+	}
 }
